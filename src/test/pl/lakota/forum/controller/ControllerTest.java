6fd4d465eb1e35f8ac5b4d8b9a5be5ff6f928cb0
Mateusz <MateuszLakota@outlook.com>
2021-01-15 23:27:12 +0100

    Implemented partial unit tests.

---
+++ src/test/pl/lakota/forum/controller/ControllerTest.java
@@ -0,0 +1,23 @@
+package java.pl.lakota.forum.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class ControllerTest {
+
+    @Test
+    void getWelcomePage() {
+        Assert.isTrue(true);
+    }
+
+    void proceedToForum() {
+    }
+
+    void goToSendMessagePage() {
+    }
+
+    void sendMessage() {
+    }
+}
